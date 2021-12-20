eb5db31cf88583468aafa9dec96870116516b2c5
Yashwanth Gowda B S <78403160+YashwanthGowda07@users.noreply.github.com>
2021-12-20 11:08:09 +0530

    Add files via upload

---
+++ Piece.java
@@ -0,0 +1,36 @@
+package ChessGame;
+
+import java.util.*;
+
+/**
+ *
+ * @author Khouiled
+ */
+public class Piece {
+    int xp;
+    int yp;
+    boolean isWhite;
+    LinkedList<Piece> ps;
+    String name;
+
+    public Piece(int xp, int yp, boolean isWhite, String n, LinkedList<Piece> ps) {
+        this.xp = xp;
+        this.yp = yp;
+        this.isWhite = isWhite;
+        this.ps = ps;
+        name = n;
+        ps.add(this);
+    }
+
+    public void move(int xp, int yp) {
+        ps.stream().filter((p) -> (p.xp == xp && p.yp == yp)).forEachOrdered((p) -> {
+            p.kill();
+        });
+        this.xp = xp;
+        this.yp = yp;
+    }
+
+    public void kill() {
+        ps.remove(this);
+    }
+}
